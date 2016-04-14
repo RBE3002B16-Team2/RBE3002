fbd0943e4a1b456df846af5cb53789089ee7ddac
Keshuai Xu <cosine@wpi.edu>
2016-04-14 02:10:06 -0400

    the transform thing does not work.

--- scripts/CostmapThing.py
+++ scripts/CostmapThing.py
@@ -3,6 +3,9 @@ import rospy
 from nav_msgs.msg import GridCells
 from geometry_msgs.msg import Twist, Point, Pose, PoseStamped, PoseWithCovarianceStamped
 from nav_msgs.msg import Odometry, OccupancyGrid, Path
+import tf
+import math
+from astar import aStar, NoPathFoundException
 
 
 class CostmapThing:
@@ -53,6 +56,118 @@ class CostmapThing:
 
         return cells
 
+    def make_navigable_gridpos(self):
+        width = self.og.info.width
+        height = self.og.info.height
+
+        # resolution and offset of the map
+        k = 0
+        cells = []
+
+        for i in range(0, height):  # height should be set to hieght of grid
+            for j in range(0, width):  # width should be set to width of grid
+                if self.onedmap[k] < 30:
+                    cells.append((j, i))
+                k += 1
+
+        return cells
+
+    # returns PoseStamped
+    def getNextWaypoint(self, start, goal, dist_limit=None, pathpub=None, wppub=None):
+        resolution = self.og.info.resolution
+        width = self.og.info.width
+        height = self.og.info.height
+        offsetX = self.og.info.origin.position.x
+        offsetY = self.og.info.origin.position.y
+
+        # it is broken here
+        # t = tf.TransformerROS(True, rospy.Duration(1.0))
+        t = tf.TransformListener()
+        tstart = t.transformPose(self.og.header.frame_id, start)
+        tgoal = t.transformPose(self.og.header.frame_id, goal)
+
+        if dist_limit is not None:
+            tgoal = limit_max_dist(tstart, tgoal, dist_limit)
+
+        if wppub is not None:
+            wppub.publish(tgoal)
+
+        start_grid_pos = pose2gridpos(tstart.pose, resolution, offsetX, offsetY)
+        goal_grid_pos = pose2gridpos(tgoal.pose, resolution, offsetX, offsetY)
+        try:
+            path = aStar(self.make_navigable_gridpos(), start_grid_pos, goal_grid_pos)
+            wp = self.getWaypoints(path, goal, pathpub)
+            retp = PoseStamped()
+            if len(wp) > 1:
+                retp.pose = wp[1].pose
+                retp.header = self.og.header
+            else:
+                retp = goal
+            return retp
+        except NoPathFoundException as e:
+            raise e
+
+    # generate a Path from a list of gridpos (x,y)
+    def getWaypoints(self, list_of_gridpos, goal_pose, publisher=None):
+        resolution = self.og.info.resolution
+        width = self.og.info.width
+        height = self.og.info.height
+        offsetX = self.og.info.origin.position.x
+        offsetY = self.og.info.origin.position.y
+
+        wp = Path()
+        wp.header.frame_id = 'map'
+        poses = []
+        lastdirection = -999
+        for i in range(0, len(list_of_gridpos) - 1):
+            direction = getDirection(list_of_gridpos[i], list_of_gridpos[i + 1])
+            if direction != lastdirection:
+                lastdirection = copy.deepcopy(direction)
+                newPose = PoseStamped()
+                newPose.header.frame_id = 'map'
+                newPose.pose = Pose()
+                newPose.pose.position = getPoint(list_of_gridpos[i], resolution, offsetX, offsetY)
+                quaternion = tf.transformations.quaternion_from_euler(0, 0, direction)
+                newPose.pose.orientation.x = quaternion[0]
+                newPose.pose.orientation.y = quaternion[1]
+                newPose.pose.orientation.z = quaternion[2]
+                newPose.pose.orientation.w = quaternion[3]
+                poses.append(newPose)
+        newPose = PoseStamped()
+        newPose.header.frame_id = 'map'
+        newPose.pose = goal_pose
+        poses.append(newPose)
+        wp.poses = poses
+        if publisher is not None:
+            publisher.publish(wp)
+        return wp.poses
+
+
+def getDirection(fr, to):
+    dx = to[0] - fr[0]
+    dy = to[1] - fr[1]
+    return math.atan2(dy, dx)
+
+
+def get_distance(fr_posestamped, to_posestamped):
+    dx = fr_posestamped.pose.position.x - to_posestamped.pose.position.x
+    dy = fr_posestamped.pose.position.y - to_posestamped.pose.position.y
+    return math.sqrt(dx ** 2 + dy ** 2)
+
+
+def get_direction(fr_posestamped, to_posestamped):
+    dx = fr_posestamped.pose.position.x - to_posestamped.pose.position.x
+    dy = fr_posestamped.pose.position.y - to_posestamped.pose.position.y
+    return math.atan2(dy, dx)
+
+
+def limit_max_dist(fr_posestamped, to_posestamped, max_dist):
+    angle = get_direction(fr_posestamped, to_posestamped)
+    if get_distance(fr_posestamped, to_posestamped) > max_dist:
+        to_posestamped.pose.position.x = fr_posestamped.pose.position.x + math.cos(angle) * max_dist
+        to_posestamped.pose.position.y = fr_posestamped.pose.position.y + math.sin(angle) * max_dist
+    return to_posestamped
+
 
 def getPoint(gridpos, resolution, offsetX, offsetY):
     point = Point()
@@ -61,3 +176,9 @@ def getPoint(gridpos, resolution, offsetX, offsetY):
     point.z = 0
 
     return point
+
+
+def pose2gridpos(pose, resolution, offsetX, offsetY):
+    gridx = int((pose.position.x - offsetX - (.5 * resolution)) / resolution)
+    gridy = int((pose.position.y - offsetY - (.5 * resolution)) / resolution)
+    return (gridx, gridy)
---
+++ scripts/lab4main.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python
+
+# Author Joseph St. Germain
+# Co-Authur Arthur lockmans drive smooth function
+
+
+import rospy, tf, numpy, math
+from kobuki_msgs.msg import BumperEvent
+from map_msgs.msg import OccupancyGridUpdate
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist, Pose
+from nav_msgs.msg import Odometry, Path, GridCells, OccupancyGrid
+from geometry_msgs.msg import PoseStamped
+from tf.transformations import euler_from_quaternion
+
+from CostmapThing import CostmapThing
+from astar import NoPathFoundException
+
+wheel_rad = 3.5 / 100.0  # cm
+wheel_base = 23.0 / 100.0  # cm
+
+
+def publishTwist(lin_Vel, ang_Vel):
+    """Send a movement (twist) message."""
+    global pub
+    msg = Twist()
+    msg.linear.x = lin_Vel
+    msg.angular.z = ang_Vel
+    pub.publish(msg)
+
+
+def navToPose(goal):
+    """Drive to a goal subscribed to from /move_base_simple/goal"""
+    # compute angle required to make straight-line move to desired pose
+    global xPosition
+    global yPosition
+    global theta
+
+    initialX = xPosition
+    initialY = yPosition
+    initialT = theta
+
+    # capture desired x and y positions
+    desiredY = goal.pose.position.y
+    desiredX = goal.pose.position.x
+    # capture desired angle
+    quat = goal.pose.orientation
+    q = [quat.x, quat.y, quat.z, quat.w]
+    roll, pitch, yaw = euler_from_quaternion(q)
+    desiredT = yaw
+
+    dx = desiredX - initialX
+    dy = desiredY - initialY
+    distance = math.sqrt((dx) ** 2 + (dy) ** 2)
+    pathT = math.atan2(dy, dx)
+
+    initialTurn = getMinimalAngleDifference(pathT, initialT)
+    finalTurn = getMinimalAngleDifference(desiredT, pathT)
+
+    print "initialTurn" + str(initialTurn)
+    print "finalTurn" + str(finalTurn)
+    print "distance" + str(distance)
+
+    print "spin!"  # turn to calculated angle
+    rotate(initialTurn)
+    print "move!"  # move in straight line specified distance to new pose
+    # driveSmooth(0.25, distance)
+
+    driveSmooth(0.25, distance)
+    rospy.sleep(2)
+    print "spin!"  # spin to final angle
+    rotate(finalTurn)
+    print "done"
+
+
+def getMinimalAngleDifference(target, source):
+    return math.atan2(math.sin(target - source), math.cos(target - source))
+
+
+def driveSmooth(speed, distance):
+    """This function accepts a speed and a distance for the robot to move in a smoothed straight line."""
+    global pose
+
+    initialX = pose.position.x
+    initialY = pose.position.y
+    atTarget = False
+    currentSpeed = 0.
+    currentDistance = 0.
+    sampleTime = 0.15
+    acceleration = 0.5  # unit/s
+    # Loop until the distance between the attached frame and the origin is equal to the
+    # distance specified
+    while (not atTarget and not rospy.is_shutdown()):
+        if (currentSpeed < speed):
+            currentSpeed = currentSpeed + acceleration / (1. / sampleTime)
+        else:
+            currentSpeed = speed
+
+        maxSpeedDeceleration = (distance - currentDistance) * 1 + 0.1
+        if (currentSpeed > maxSpeedDeceleration):
+            currentSpeed = maxSpeedDeceleration
+
+        currentX = pose.position.x
+        currentY = pose.position.y
+        currentDistance = math.sqrt((currentX - initialX) ** 2 + (currentY - initialY) ** 2)  # Distance formula
+        if (currentDistance >= distance):
+            atTarget = True
+            publishTwist(0, 0)
+        else:
+            publishTwist(currentSpeed, 0)
+            rospy.sleep(sampleTime)
+
+
+def rotate(angle):
+    global odom_list
+    global pose
+    vel = Twist();
+    done = True
+    targetAngle = pose.orientation.z + angle
+    error = getMinimalAngleDifference(targetAngle, pose.orientation.z)
+    while ((abs(error) >= 0.05) and not rospy.is_shutdown()):
+        publishTwist(0, numpy.sign(error) * math.pi * 0.2)
+        rospy.sleep(0.15)
+        error = getMinimalAngleDifference(targetAngle, pose.orientation.z)
+    publishTwist(0, 0)
+
+
+# keeps track of current location and orientation
+def tCallback(event):
+    global pose
+    global xPosition
+    global yPosition
+    global theta
+
+    odom_list.waitForTransform('map', 'base_footprint', rospy.Time(0), rospy.Duration(1.0))
+    (position, orientation) = odom_list.lookupTransform('map', 'base_footprint', rospy.Time(0))
+    pose.position.x = position[0]
+    pose.position.y = position[1]
+    # the previous 2 lines and next 2 lines are repedative. Joes bad
+    xPosition = position[0]
+    yPosition = position[1]
+
+    odomW = orientation
+    q = [odomW[0], odomW[1], odomW[2], odomW[3]]
+    roll, pitch, yaw = euler_from_quaternion(q)
+    # convert yaw to degrees
+    pose.orientation.z = yaw
+    theta = yaw
+
+
+def pathToPoints(list_of_pos):
+    print "Path to points..."
+    for pose in list_of_pos.poses:
+        navToPose(pose)
+
+
+def rawwwrrr(goal):
+    global pose
+    global globalCostmapThing
+    global localCostmapThing
+
+    ps = PoseStamped()
+    ps.pose = pose
+    ps.header.frame_id='/map'
+
+    pubrealpath = rospy.Publisher("/realpath", Path, queue_size=1)
+
+    done = False
+
+    nextwp = None
+
+    try:
+        nextwp = globalCostmapThing.getNextWaypoint(ps, goal, pathpub=pubrealpath)
+        while not done:
+            # if close enough kill
+            try:
+                nextwp = localCostmapThing.getNextWaypoint(ps, nextwp, pathpub=pubrealpath)
+                navToPose(nextwp)
+            except NoPathFoundException:
+                nextwp = globalCostmapThing.getNextWaypoint(ps, goal, pathpub=pubrealpath)
+
+    except NoPathFoundException:
+        print 'it is not possible'
+
+
+# This is the program's main function
+if __name__ == '__main__':
+    rospy.init_node('lab4')
+    global pub
+    global odom_list
+
+    global pose
+    global globalCostmapThing
+    global localCostmapThing
+    # global odom_tf
+    pose = Pose()
+
+    goal_sub = rospy.Subscriber('/move_base_simple/goalrbe', PoseStamped, rawwwrrr, queue_size=1)
+
+    globalCostmapThing = CostmapThing()
+    localCostmapThing = CostmapThing()
+
+    pub_local = rospy.Publisher("/navigable_points_local", GridCells, queue_size=1)
+    pub_global = rospy.Publisher("/navigable_points_global", GridCells, queue_size=1)
+
+    sub_local = rospy.Subscriber("/move_base/local_costmap/costmap", OccupancyGrid, localCostmapThing.map_cb)
+    sub_local_update = rospy.Subscriber("/move_base/local_costmap/costmap_updates", OccupancyGridUpdate,
+                                        localCostmapThing.map_update_cb, callback_args=pub_local)
+
+    sub_global = rospy.Subscriber("/move_base/global_costmap/costmap", OccupancyGrid, globalCostmapThing.map_cb)
+    sub_global_update = rospy.Subscriber("/move_base/global_costmap/costmap_updates", OccupancyGridUpdate,
+                                         globalCostmapThing.map_update_cb, callback_args=pub_global)
+
+    rospy.Timer(rospy.Duration(.01), tCallback)  # timer callback for robot location
+
+    odom_list = tf.TransformListener()  # listner for robot location
+
+    print "Starting Lab 4"
+    while not rospy.is_shutdown():
+        rospy.spin()
