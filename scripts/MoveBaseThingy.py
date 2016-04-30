40802d48d7889b96087b8ef498470fd162aebead
Keshuai Xu <cosine@wpi.edu>
2016-04-30 18:44:06 -0400

    most things work. need spin

--- scripts/MoveBaseThingy.py
+++ scripts/MoveBaseThingy.py
@@ -1,14 +1,33 @@
+import rospy
+import actionlib
+import tf
+from move_base_msgs.msg import MoveBaseGoal, MoveBaseAction
+from geometry_msgs.msg import Pose, Point
+from actionlib_msgs.msg import GoalStatus
+
+
 class MoveBaseThingy:
     def __init__(self):
-        pass
+        self.movebase = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+        print 'waiting for move base...'
+        self.movebase.wait_for_server()
+        print 'move base action server found'
+
 
     '''
     PoseStamped goal
     returns int GoalStatus from the status
     This method should block until nav done or aborted
     '''
-    def go_to(self, goal, timeout=0):
-        pass
+    def go_to(self, goal_posestamped, timeout=0):
+        goal = MoveBaseGoal()
+        goal.target_pose = goal_posestamped
+        self.movebase.send_goal(goal)
+        self.movebase.wait_for_result()  # timeout maybe?
+        result = self.movebase.get_goal_status_text()
+        print 'move_base ' + result
+        resultcode = self.movebase.get_state()
+        return resultcode
 
 
     '''
--- scripts/finalmain.py
+++ scripts/finalmain.py
@@ -14,13 +14,46 @@ from FuckTheTime import fuck_the_time
 
 
 from FrontierExplorer import FrontierExplorer
+from MoveBaseThingy import MoveBaseThingy
+from actionlib_msgs.msg import GoalStatus
 
 class Final3002:
     def __init__(self):
         self.tfl = tf.TransformListener()
         self.fe = FrontierExplorer(tf_listener=self.tfl)
+        self.move = MoveBaseThingy()
 
-        self.nav_goal_candidates_pub = rospy.Publisher("/next_goal", PoseStamped, queue_size=1)
+        self.next_goal_pub = rospy.Publisher("/next_goal", PoseStamped, queue_size=1)
+
+    def explore(self):
+        print 'start explore'
+        points = self.fe.get_nav_goal_point_candidates()
+
+        while points:
+            r = self.go_to_first_feasible_point(points)
+            if r is False:
+                print "Got stuck. give up"
+                return
+            points = self.fe.get_nav_goal_point_candidates()
+
+        print 'done explore'
+
+    def go_to_first_feasible_point(self,points):
+        while points:
+            p = points.pop(0)
+            next_goal = point2pose(p)
+            self.next_goal_pub.publish(next_goal)
+            result = self.move.go_to(next_goal)
+            if result in [GoalStatus.SUCCEEDED, GoalStatus.ABORTED]:
+                return True
+            elif result in [GoalStatus.REJECTED]:
+                continue
+            else:
+                print "This should not happen. move_base result " + repr(result)
+
+        print "None of the points are feasible"
+        return False
+
 
     def test_get_goal_points(self,event):
         print "test get goal points"
@@ -31,16 +64,30 @@ class Final3002:
         next_goal.header.frame_id = 'map'
         next_goal = fuck_the_time(next_goal)
 
-        self.nav_goal_candidates_pub.publish(next_goal)
+        self.next_goal_pub.publish(next_goal)
 
 
+def point2pose(point):
+    next_goal = PoseStamped()
+    next_goal.pose.position = point
+    q = tf.transformations.quaternion_from_euler(0, 0, 0)
+    next_goal.pose.orientation.x = q[0]
+    next_goal.pose.orientation.y = q[1]
+    next_goal.pose.orientation.z = q[2]
+    next_goal.pose.orientation.w = q[3]
+    next_goal.header.frame_id = 'map'
+    next_goal = fuck_the_time(next_goal)
+    return next_goal
 
 if __name__ == '__main__':
     print "Starting RBE3002 final"
     rospy.init_node('rbe3002_final')
     inst = Final3002()
 
-    rospy.Timer(rospy.Duration(5), inst.test_get_goal_points)
+    # rospy.Timer(rospy.Duration(5), inst.test_get_goal_points)
+
+    # rospy.sleep(1)
+    inst.explore()
 
     while not rospy.is_shutdown():
         rospy.spin()
