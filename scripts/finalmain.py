49b735822e7919397638be27f32b29d9cb2279a1
Keshuai Xu <cosine@wpi.edu>
2016-04-30 15:25:29 -0400

    skeleton of movebasethingy

--- scripts/finalmain.py
+++ scripts/finalmain.py
@@ -6,5 +6,7 @@ if __name__ == '__main__':
     print "Starting RBE3002 final"
     rospy.init_node('rbe3002_final')
     fe = FrontierExplorer()
+    rospy.sleep()
     while not rospy.is_shutdown():
         rospy.spin()
+
